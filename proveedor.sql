38fcde81678b85826db9e1d8398ac8973d2ae888
SojeGod <114420938+SojeGod@users.noreply.github.com>
2022-09-26 10:05:54 -0500

    carga inicial

---
+++ proveedor.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-09-2022 a las 06:19:46
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `pieza`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `idProveedor` int(10) UNSIGNED NOT NULL,
+  `Snum` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `Snombre` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Estado` int(10) UNSIGNED NOT NULL,
+  `Ciudad_proveedor` varchar(50) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `proveedor`
+--
+
+INSERT INTO `proveedor` (`idProveedor`, `Snum`, `Snombre`, `Estado`, `Ciudad_proveedor`) VALUES
+(1, 'S1', 'Salazar', 20, 'Londres'),
+(2, 'S2', 'Jaimes', 10, 'Paris'),
+(3, 'S3', 'Bernal', 30, 'Paris'),
+(4, 'S4', 'Corona', 40, 'Londres'),
+(5, 'S5', 'Aldana', 30, 'Atenas');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`idProveedor`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  MODIFY `idProveedor` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
